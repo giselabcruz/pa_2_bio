93d4acc6f6d1afae81f7ea287cc9433f519ed1d8
giselabcruz <giselaisabelbelmontecruz@gmail.com>
2025-09-30 15:50:26 +0100

    Data path restructured

--- ejercicios/ejercicio1.py
+++ ejercicios/ejercicio1.py
@@ -1,8 +1,5 @@
 from Bio.Seq import Seq
 
-def header(title: str) -> None:
-    print(f"\n{'_'*5} {title} {'_'*5}")
-
 def complement(seq: str) -> Seq:
     return Seq(seq).complement()
 
--- ejercicios/ejercicio2.py
+++ ejercicios/ejercicio2.py
@@ -1,6 +1,8 @@
 from Bio import SeqIO
 
-def transcribe_gene(fasta_path: str = "mocked_gene.fna"):
+fasta = "data/mocked_gene.fna"
+
+def transcribe_gene(fasta_path: str = fasta):
     """Lee un FASTA y devuelve ADN, ARNm y ARNm inverso."""
     record = next(SeqIO.parse(fasta_path, "fasta"))
     dna = record.seq
@@ -9,8 +11,6 @@ def transcribe_gene(fasta_path: str = "mocked_gene.fna"):
     return dna, mrna, mrna_reverse
 
 def main():
-    print("_____Ejercicio 2_____")
-    fasta = "data/mocked_gene.fna"
     dna, mrna, mrna_reverse = transcribe_gene(fasta)
 
     print("Secuencia ADN completa:")
