f7db4927c1028db574eb80ea984d58cadfc1c3c3
giselabcruz <giselaisabelbelmontecruz@gmail.com>
2025-09-30 15:35:24 +0100

    fix typo

--- ejercicios/ejercicio1.py
+++ ejercicios/ejercicio1.py
@@ -1,7 +1,7 @@
 from Bio.Seq import Seq
 
-def header(titulo: str) -> None:
-    print(f"\n{'_'*5} {titulo} {'_'*5}")
+def header(title: str) -> None:
+    print(f"\n{'_'*5} {title} {'_'*5}")
 
 def complement(seq: str) -> Seq:
     return Seq(seq).complement()
